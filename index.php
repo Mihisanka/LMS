b9823b8c5f9ef9f0976bd2dd4caaea9d895e318d
Mihisanka Sandudeeptha <mihisanka1998@gmail.com>
2024-01-26 12:28:15 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Student Management System </title>
+    <link rel="stylesheet" href="style.css">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css"
+        integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
+    </script>
+</head>
+
+<body>
+    <nav>
+        <label class="logo">W-School</label>
+
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Admission</a></li>
+            <li><a href="" class="btn btn-success">Login</a></li>
+        </ul>
+    </nav>
+
+    <div class="section1">
+        <img class="main_img" src="main-bg.jpg">
+        <label class="main_text"> we learn at this collage</label>
+    </div>
+
+    <center>
+        <h1>Welcome to W-school </h1>
+    </center>
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-4">
+
+                <img class="welcome_img" src="school2.jpg">
+
+                <div class="col-md-8">
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <center>
+        <h1>Teachers</h1>
+    </center>
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-4">
+                <img class="teacher" src="teacher1.jpg">
+            </div>
+            <div class="col-md-4">
+                <img class="teacher" src="teacher2.jpg">
+            </div>
+
+            <div class="col-md-4">
+                <img class="teacher" src="teacher3.jpg">
+            </div>
+
+        </div>
+    </div>
+
+    <center>
+        <h1>Causes </h1>
+    </center>
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-4">
+                <img class="Causes" src="school.jpg">
+            </div>
+            <div class="col-md-4">
+                <img class="Causes" src="marketing.png">
+            </div>
+
+            <div class="col-md-4">
+                <img class="Causes" src="web.jpg">
+            </div>
+
+        </div>
+    </div>
+</body>
+
+</html>
