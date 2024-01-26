3ad7853240467fdcb2060fd81f4e46479bb50eec
Mihisanka Sandudeeptha <mihisanka1998@gmail.com>
2024-01-26 19:19:01 +0530

    login form
    f-e of login form

---
+++ login.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Login Form</title>
+    <link rel="stylesheet" href="style.css">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css"
+        integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
+    </script>
+</head>
+
+<body>
+    <center>
+        <div class=" form_deg">
+            <form class="login_fo">
+                <center class="title_log">
+                    Login form
+                </center>
+                <div>
+                    <label class="label_text">Username</label>
+                    <input type="text" name="username">
+                </div>
+                <div>
+                    <label class="label_text">Password</label>
+                    <input type="text" name="username">
+                </div>
+                <div>
+
+                    <input class="btn btn-primary" type="submit" name="login" value="Login">
+                </div>
+            </form>
+
+        </div>
+    </center>
+</body>
+
+</html>
