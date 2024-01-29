04a82112491ff78b5091f16f494afee26ece5881
Mihisanka Sandudeeptha <mihisanka1998@gmail.com>
2024-01-29 09:57:36 +0530

    admission
    admission data update done

---
+++ data_check.php
@@ -0,0 +1,26 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "lms-php";
+
+$data = mysqli_connect($host, $user, $password, $dbname);
+
+if (isset($_POST['apply'])) {
+    $data_name = $_POST['name'];
+    $data_email = $_POST['email'];
+    $data_phone = $_POST['phone'];
+    $data_message = $_POST['message'];
+
+    $sql = "INSERT INTO admission (name, email, phone, message) 
+            VALUES ('$data_name', '$data_email', '$data_phone', '$data_message')";
+
+    $result = mysqli_query($data, $sql);
+
+    if ($result) {
+        echo "apply success";
+    } else {
+        echo "apply failed";
+    }
+}
+?>
--- index.php
+++ index.php
@@ -116,25 +116,25 @@
     </center>
 
     <div align="center" class="admission_from">
-        <form>
+        <form action="data_check.php" method="POST">
             <div class="adm_int">
                 <label class="label_text">Name</label>
-                <input class="input_deg" type="text">
+                <input class="input_deg" type="text" name="name">
             </div>
             <div>
                 <label class="label_text">Email</label>
-                <input class="input_deg" type="text">
+                <input class="input_deg" type="text" name="email">
             </div>
             <div>
                 <label class="label_text">Phone</label>
-                <input class="input_deg" type="text">
+                <input class="input_deg" type="text" name="phone">
             </div>
             <div>
                 <label class="label_text">Message</label>
-                <textarea class="input_text"></textarea>
+                <textarea class="input_text" name="message"></textarea>
             </div>
             <div class="adm_int">
-                <input class="btn btn-primary" id="submit" type="submit" value="Apply">
+                <input class="btn btn-primary" id="submit" type="submit" value="Apply" name="apply">
             </div>
         </form>
     </div>
